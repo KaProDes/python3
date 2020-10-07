4d1cfe48a7d6a24997276a0e20e93aaabb826be5
Kapil Pramod Deshmukh <44638458+KaProDes@users.noreply.github.com>
2020-10-08 00:52:04 +0530

    Create checklive.py

---
+++ Tutorial/checklive.py
@@ -0,0 +1,13 @@
+import requests,os,time
+
+SITE_URL=""
+SLEEP_SEC=300 #5 mins
+while True:
+    response=requests.get(SITE_URL)
+    print(response)
+    if response.status_code!=200:
+          os.system('notify-send "Website Down Now" "'+SITE_URL+'is down"')
+          break
+          
+          
+    time.sleep(SLEEP_SEC)
